66959905f33b20a6936836aa389f37746c501316
Arkadiusz <swiokloarkadiusz@gmail.com>
2020-03-18 11:16:11 +0100

    refactored tests

--- src/test/java/pl/swioklo/arkadiusz/java/carsharing/service/mapper/CarMapperTest.java
+++ src/test/java/pl/swioklo/arkadiusz/java/carsharing/service/mapper/CarMapperTest.java
@@ -1,5 +1,6 @@
 package pl.swioklo.arkadiusz.java.carsharing.service.mapper;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.swioklo.arkadiusz.java.carsharing.dao.entity.CarEntity;
 import pl.swioklo.arkadiusz.java.carsharing.web.model.CarModel;
@@ -10,15 +11,25 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class CarMapperTest {
+    CarMapper carMapper;
+    CarModel carModel;
+    CarEntity carEntity;
+    
+    
+    @BeforeEach
+    void setUp() {
+        CarMapper carMapper = new CarMapper();
+        CarModel carModel = new CarModel();
+        CarEntity carEntity = new CarEntity();
+    }
     
     @Test
     void givenCarMapper_whenFromModelToEntity_thenCarEntityNotNull() {
         //given
-        CarMapper carMapper = new CarMapper();
-        CarModel carModel = new CarModel();
+        
         
         //when
-        CarEntity carEntity = carMapper.fromModelToEntity(carModel);
+        carEntity = carMapper.fromModelToEntity(carModel);
         
         //then
         assertNotNull(carEntity, "carEntity is null");
@@ -28,8 +39,6 @@ class CarMapperTest {
     @Test
     void givenCarMapper_whenFromEntityToModel_thenCarModelNotNull() {
         //given
-        CarMapper carMapper = new CarMapper();
-        CarEntity carEntity = new CarEntity();
         
         //when
         CarModel carModel = carMapper.fromEntityToModel(carEntity);
@@ -41,25 +50,24 @@ class CarMapperTest {
     @Test
     void givenCarMapper_whenFromEntitiesToModels_thenCarModelsNotNull() {
         //given
-        CarMapper carMapper = new CarMapper();
+   
         List<CarEntity> carEntity = new ArrayList<>();
         
         //when
         List<CarModel> carModels = carMapper.fromEntitiesToModels(carEntity);
-    
+        
         //then
-        assertNotNull(carModels,"carModelList is null");
+        assertNotNull(carModels, "carModelList is null");
     }
     
     @Test
-    void given_when_then(){
+    void givenCarMapper_whenFromModelsToEntities_thenCarEntitiesNotNull() {
         //given
-        CarMapper carMapper = new CarMapper();
         List<CarModel> carModels = new ArrayList<>();
         
         //when
         List<CarEntity> carEntities = carMapper.fromModelsToEntities(carModels);
-    
+        
         //then
         assertNotNull(carEntities, "carEntities is null");
     }
